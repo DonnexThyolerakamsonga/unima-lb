155e14c24f93230db63dc5202833bda5b1e0895d
dthyolerak <dthyolerak1.com@mail.com>
2021-12-22 00:59:50 +0200

    returning json in testing

--- src/main/java/com/mvaka/unimalb/UnimaLbApplication.java
+++ src/main/java/com/mvaka/unimalb/UnimaLbApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class UnimaLbApplication {
@@ -13,7 +15,7 @@ public class UnimaLbApplication {
 		SpringApplication.run(UnimaLbApplication.class, args);
 	}
     @GetMapping
-        public String Testing(){
-            return "Testing Testing hello world";
+        public List<String> Testing(){
+            return List.of("Testing Testing hello world", "Testing Testing hello world","Testing Testing hello world");
         }
 }
